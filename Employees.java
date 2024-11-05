8df395abb5358eb14d099056cc0bd137ef669aaa
proche13 <148491969+proche13@users.noreply.github.com>
2024-11-05 11:44:36 +0000

    Add files via upload

---
+++ Employees.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class Employees{
+    String employeeID;
+    String name;
+    String position;
+    String salaryScale;
+    int currentPoint;
+    boolean isFullTime;
+    List<Payslip> payslips = new ArrayList<Payslip>();
+    List<employees> employees = new ArrayList<employees>();
+
+    public Employee(String employeeID, String name, String position, String salaryScale, int currentPoint, boolean isFullTime, List<Payslip> payslips, List<employees> employees){
+        this.employeeID = employeeID;
+        this.name = name;
+        this.position = position;
+        this.salaryScale = salaryScale;
+        this.currentPoint = currentPoint;
+    }
+
+    public String getEmployeeID(){
+        return employeeID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPosition(){
+        return position;
+    }
+
+    public String getSalaryScale(){
+        return salaryScale;
+    }
+
+    public int getCurrentPoint(){
+        return currentPoint;
+    }
+
+    public boolean isFullTime(){
+        if(isFullTime == true){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void setEmployeeID(String employeeID){
+        this.employeeID = employeeID;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    public void setSalaryScale(int salaryScale){
+        this.salaryScale = salaryScale;
+    }
+
+    public void set
+}
