b6691b3e8236e0418f1705904b4c7f890e3e6764
Michal-Czek <michal.czekalski.04@gmail.com>
2024-11-07 15:47:12 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,42 +1,45 @@
-public class Admin Extends Employee {
-    public  Admin(String EmployeeID,String name,String position){
-    super (String EmployeeID, String name, String position);//pulls parameters from employees
+import java.util.ArrayList;
+
+public class Admin extends Employees {
+
+    private static ArrayList<Employees> employees = new ArrayList<Employees>(); // Static employee list
+
+    // Admin constructor - Calls Employees constructor
+    public Admin(String employeeID, String name, String position, String salaryScale ,int currentPoint, boolean isFullTime) {
+        super(employeeID, name, position, salaryScale, currentPoint, isFullTime); // Calls Employees to get parameters
     }
 
-    //add employee adds an employee to the Employess class,it doesnt use public
-    //as it only can be used by admin.
+    // Method to add an employee
+    public void addEmployee(Employees employee) {
+        employees.add(employee); // Add employee to the list
+    }
 
-    private void addEmployee(Employee employee, String position) {
-           double Salary = newSalary Salary.setSalary(position);//gets the salary for the given position
-
-           employee.setPosition(position);//sets position,salary and if fulltime or not
-           Salary.setSalary(newSalary);
-           employee.isFulltime(true);
-
-           employess.add(employee);//adds to employee array
-        }
-
-        //same as addemployee except does the oppisite
-    private void removeEmployee(String  EmployeeID){
-        for (int i =0; i< employess.size(); i++) {
-            if (employees.get(i).getID().equals(EmployeeID)) {//gets index (i) and checks if this ID is equal to the ID of the given ID
-                employees.remove(i);//removes the ID at array index (i)
-                i--; // if an item was removed,array shrinks so this would make sure we dont skip a item in the array
-                return; //This stops the loop if an employee was removed
+    // Method to remove an employee by their employee ID
+    public void removeEmployee(String employeeID) {
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getEmployeeID().equals(employeeID)) {
+                employees.remove(i);
+                // Remove employee by ID
+                System.out.println("Employee " + employeeID + " removed.");
+                i--;
+                return;
             }
         }
-        System.out.println("Employee not found");//If the loop stops with no id found,prints this message
+        System.out.println("Employee not found."); // If not found
     }
 
-    //this promotes a employee by getting their ID while the position parameter is what position you want to promote
-    // them to be. It then gets a new salary inline with the new position then sets both of them.
+    // Method to promote an employee
+    public void promoteEmployee(String employeeID, String newPosition) {
+        for (int i = 0; i < employees.size(); i++) {
+            if (employees.get(i).getEmployeeID().equals(employeeID)) {
+                employees.get(i).setPosition(newPosition); // Set new position
+                System.out.println("Employee " + employeeID + " promoted to " + newPosition);
 
-    private void Promotion(String EmployeeID,String position){
-        Employee employee = getEmployeeID(EmployeeID);
-        double newSalary = Salary.getSalary(position);
-        double Salary = Salary.setSalary(newSalary);
-        String postition = Employee.setPosition(position);
-
+                return;
+            }
+        }
+        System.out.println("Employee not found for promotion.");
     }
+
 
 }
