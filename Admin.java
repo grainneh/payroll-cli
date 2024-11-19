69199309bd0c031f5e6caa95c6c2d3a106288d1f
Michal-Czek <michal.czekalski.04@gmail.com>
2024-11-19 10:28:31 +0000

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,17 +1,28 @@
+/*
+This class uses two methods:one to remove and one to add employees to the employees array.
+addEmployee simply uses .add() to add an employee to the array. RemoveEmployee uses a for loop to
+go through the array and find an ID that matches the given ID. It also has a getEmployees method so you can make sure
+the add/remove worked correctly.
+
+@author Michal Czekalski
+ */
+
+
+
+import java.io.FileWriter;
 import java.util.ArrayList;
 
-public class Admin extends Employees {
+public class Admin  {
 
-    private static ArrayList<Employees> employees = new ArrayList<Employees>(); // Static employee list
+    private static ArrayList<Employee> employees = new ArrayList<Employee>(); // Static employee list
 
-    // Admin constructor - Calls Employees constructor
-    public Admin(String employeeID, String name, String position, String salaryScale ,int currentPoint, boolean isFullTime) {
-        super(employeeID, name, position, salaryScale, currentPoint, isFullTime); // Calls Employees to get parameters
-    }
+
+
 
     // Method to add an employee
-    public void addEmployee(Employees employee) {
-        employees.add(employee); // Add employee to the list
+    public void addEmployee(Employee employee) {
+        employees.add(employee);// Add employee to the list
+        System.out.println("Employee "+employee.getName()+" added");
     }
 
     // Method to remove an employee by their employee ID
@@ -28,11 +39,20 @@ public class Admin extends Employees {
         System.out.println("Employee not found."); // If not found
     }
 
-    public static ArrayList<Employees> getEmployees() {
+    public static ArrayList<Employee> getEmployees() {
         return employees;
     }
 
+public static void setEmployees(ArrayList<Employee> employees) {
+        Admin.employees = employees;
 
+}
+
+public void  updateCSV(ArrayList<Employee> employees){
+    String filePath = "emplpoyees.csv";
+
+
+}
 
 
 }
