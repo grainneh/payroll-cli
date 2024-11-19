f7557a6855f0b2464fa440c719ec93e134c709b5
grainneh <grainne.hartigan@gmail.com>
2024-11-19 10:54:18 +0000

    Update Employees.java

--- Employees.java
+++ Employees.java
@@ -1,72 +1,73 @@
 import java.util.ArrayList;
-
-public class Employee{
+public class Employee {
     String employeeID;
     String name;
     String position;
     String salaryScale;
     int currentPoint;
-    boolean isFullTime;
-    ArrayList<Payslip> payslips = new ArrayList<Payslip>();
-
-    public Employee(String employeeID, String name, String position, String salaryScale, int currentPoint, boolean isFullTime, List<Payslip> payslips, List<employees> employees){
+    boolean isFullTime; // Changed to instance variable
+    public static ArrayList<Payslip> payslips = new ArrayList<Payslip>();
+    // Constructor
+    public Employee(String employeeID, String name, String position, String salaryScale, int currentPoint, boolean isFullTime) {
         this.employeeID = employeeID;
         this.name = name;
         this.position = position;
         this.salaryScale = salaryScale;
         this.currentPoint = currentPoint;
+        this.isFullTime = isFullTime; // Initialize based on constructor input
     }
-
-    public String getEmployeeID(){
+    // Getter and Setter methods
+    public String getEmployeeID() {
         return employeeID;
     }
-
-    public String getName(){
+    public String getName() {
         return name;
     }
-
-    public String getPosition(){
+    public String getPosition() {
         return position;
     }
-
-    public String getSalaryScale(){
+    public String getSalaryScale() {
         return salaryScale;
     }
-
-    public int getCurrentPoint(){
+    public int getCurrentPoint() {
         return currentPoint;
     }
-
-    public boolean isFullTime(){
-        if(isFullTime == true){
-            return true;
-        }else {
-            return false;
-        }
+    public boolean getIsFullTime() {
+        return isFullTime; // Return instance variable
     }
-
-    public String printFullTime(){
-        if(isFullTime == true){
-            return "Full Time";
-        }else{
-            return "Part Time";
-        }
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
     }
-
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
-
-    public void setPosition(String position){
+    public void setPosition(String position) {
         this.position = position;
     }
-
-    public void setSalaryScale(String salaryScale){
+    public void setSalaryScale(String salaryScale) {
         this.salaryScale = salaryScale;
+    }
+    public void setIsFullTime(boolean isFullTime) {
+        this.isFullTime = isFullTime; // Set instance variable
+    }
+    public String printFullTime(){
+        if(isFullTime == true){
+            return "Employee is full-time";
+        }else{
+            return "Employee is part-time";
+        }
+    }
+
+
+    // Add payslip to an employee's payslip list
+    public void addPayslip(Payslip payslip) {
+        payslips.add(payslip);
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Employee ID: " + employeeID +"\n" + "Name: " + name + "\n" + "Position: " + position + "\n" + "Salary Scale: " + salaryScale + "\n" + "Current Point: " + currentPoint + "\n" + printFullTime();
     }
+
+
 }
