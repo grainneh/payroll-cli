5be915d8fa9712c91b3baf65ab7696562c6acf4a
grainneh <grainne.hartigan@gmail.com>
2024-11-29 16:21:47 +0000

    Update HumanResources.java comments

--- HumanResources.java
+++ HumanResources.java
@@ -1,9 +1,19 @@
+/**
+ * The HumanResources class has functionality to promote employees and give part-time employees a pay raise
+ *
+ * @author Michał Czekalski
+ */
+
 import java.util.ArrayList;
 
 public class HumanResources {
-    private Admin admin = new Admin();
+    private Admin admin = new Admin(); //instance of the admin class
 
-    // Method to promote an employee by employeeID
+    /**
+     * The method promoteEmployee() promotes an employee to a new position given an employee ID
+     * @param employeeID    the ID of the employee to be promoted
+     * @param newPosition   the new position
+     */
     public void promoteEmployee(String employeeID, String newPosition) {
         // Loop through the employees to find the employee by their ID
         ArrayList<Employee> employees = Admin.getEmployees();
@@ -16,11 +26,17 @@ public class HumanResources {
                 return; // Exit method once promotion is done
             }
         }
-
         // If no employee is found with the given ID
         System.out.println("Employee not found for promotion.");
     }
 
+    /**
+     * The method employeeRaise() implements functionality to give a part-time employee a pay raise, by changing their 
+     * payRate
+     * 
+     * @param employeeID    the ID of the employee to get a raise
+     * @param payRate       the new payRate
+     */
     public void employeeRaise(String employeeID, double payRate) {
         // Loop through the employees to find the employee by their ID
         ArrayList<Employee> employees = Admin.getEmployees();
@@ -32,9 +48,7 @@ public class HumanResources {
                 return; // Exit method once raise is done
             }
         }
-
         // If no employee is found with the given ID
         System.out.println("Employee not found for raise.");
     }
-
 }
